3e7026bb4f05ad306489a72a08f19b35b07dc500
IT24102877 <dgalappaththi.20@gmail.com>
2026-04-22 11:32:39 +0530

    Create PinkPetalsApplication.java

---
+++ backend/src/main/java/com/SmartCommerce/PinkPetalsApplication.java
@@ -0,0 +1,11 @@
+package com.SmartCommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PinkPetalsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PinkPetalsApplication.class, args);
+    }
+}
