71c1c9544b18adfb427307ba37a5350d0545ac1e
NimsaraVidushi <nimsaravidushi156@gmail.com>
2026-04-26 17:32:54 +0530

    Create Product.js

---
+++ frontend/src/types/Product.js
@@ -0,0 +1,32 @@
+export const ProductType = {
+    id: null,
+    name: '',
+    brand: '',
+    description: '',
+    price: 0,
+    category: '',
+    stockQuantity: 0,
+    imageUrl: '',
+    createdAt: null,
+    updatedAt: null,
+};
+
+export const CreateProductRequestType = {
+    name: '',
+    brand: '',
+    description: '',
+    price: 0,
+    category: '',
+    stockQuantity: 0,
+    imageUrl: '',
+};
+
+export const UpdateProductRequestType = {
+    name: '',
+    brand: '',
+    description: '',
+    price: null,
+    category: '',
+    stockQuantity: null,
+    imageUrl: '',
+};
